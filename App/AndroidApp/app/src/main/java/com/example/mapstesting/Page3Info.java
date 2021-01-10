0d71b925531722f9490750883774847387620a23
Kaleb <kaleb.e.hui@gmail.com>
2021-01-10 00:21:39 -0800

    added part of 2nd and 3rd page

---
+++ App/AndroidApp/app/src/main/java/com/example/mapstesting/Page3Info.java
@@ -0,0 +1,15 @@
+package com.example.mapstesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Page3Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page3);
+
+
+    }
+}
