8c14f0b654da3da166aec0a1dd5f921c72f92da1
Kaleb <kaleb.e.hui@gmail.com>
2021-01-10 11:44:52 -0800

    restructured code in page 2 and 3

--- App/AndroidApp/app/src/main/java/com/example/mapstesting/Page2List.java
+++ App/AndroidApp/app/src/main/java/com/example/mapstesting/Page2List.java
@@ -2,10 +2,13 @@ package com.example.mapstesting;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,48 +22,47 @@ public class Page2List extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
-
-        //creates a new button for each restaurant nearby
-//        for (Restaurant r : restList){
-//            LinearLayout mainLayout = findViewById(R.id.ButtonLayout);
-//
-//            Button newButton = new Button(this);
-//            newButton.setId(i);
-//            buttonList.add(newButton.getId())
-
-              //NEED TO DO SOMETHING IF NAMES ARE TOO LONG
-//            newButton.setText(r.getName);
-//
-//            mainLayout.addView(newButton);
-//        }
+        LinearLayout mainLayout = findViewById(R.id.ButtonLayout);
+        List<Integer> buttonList = new ArrayList<>();
 
 //        //display some text if there are no restaurants nearby
 //        if(!restList.isEmpty){
-//
+//            TextView error = new TextView(this);
+//            error.setGravity(Gravity.CENTER | Gravity.TOP);
+//            error.setText("No Restaurants were Found");
+//            mainLayout.addView(error);
+//        } else {
+//            //creates a new button for each restaurant nearby
+//            int i = 0;
+//            for (Restaurant r : restList){
+//                Button newButton = new Button(this);
+//                newButton.setId(i);
+//                buttonList.add(newButton.getId());
+//                mainLayout.addView(newButton);
+//                i++;
+//            }
 //        }
 
-        List<Integer> buttonList = new ArrayList<>();
-
         //below is temporary code until we can get data from API
-        for (int i = 0; i < 50; i++) {
-
-            LinearLayout mainLayout = findViewById(R.id.ButtonLayout);
-
-            Button newButton = new Button(this);
-            newButton.setText("Restaurant " + i);
-
-            newButton.setId(i);
-            buttonList.add(newButton.getId());
-            mainLayout.addView(newButton);
-        }
+//        for (int i = 0; i < 50; i++) {
+//
+//            Button newButton = new Button(this);
+//            newButton.setText("Restaurant " + i);
+//
+//            newButton.setId(i);
+//            buttonList.add(newButton.getId());
+//            mainLayout.addView(newButton);
+//        }
 
 
         for (Integer b : buttonList){
             button = findViewById(b);
+//            Restaurant chosen = restList.get(button.getId());
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    infoRestaurant();
+                    infoRestaurant(); //delete this line later
+//                    infoRestaurant(chosen);
                 }
             });
         }
@@ -69,8 +71,13 @@ public class Page2List extends AppCompatActivity {
     /**
      * moves to info page (page 3)
      */
-    public void infoRestaurant(){
+//    public void infoRestaurant(Restaurant chosen){
+    public void infoRestaurant(){ //delete this line later
         Intent intent = new Intent(this, Page3Info.class);
+//        intent.putExtra("Name", chosen.getName());
+//        intent.putExtra("curr_pop", chosen.getCurrent_popularity());
+//        intent.putExtra("rating", chosen.getRating());
+//        intent.putExtra("address", chosen.getRating());
         startActivity(intent);
     }
 }
--- App/AndroidApp/app/src/main/java/com/example/mapstesting/Page3Info.java
+++ App/AndroidApp/app/src/main/java/com/example/mapstesting/Page3Info.java
@@ -13,6 +13,8 @@ public class Page3Info extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
 
+        Bundle extras = getIntent().getExtras();
+
         LinearLayout mainLayout = findViewById(R.id.ButtonLayout);
         LinearLayout txtLayout = findViewById(R.id.textLayout);
 
@@ -20,48 +22,48 @@ public class Page3Info extends AppCompatActivity {
         ImageView ratingImage = findViewById(R.id.imageView_pos_stars);
 
         //display number of stars for restaurant
-//        int rating = (int) Math.round(restaurant.getRating());
-//        switch (rating) {
-//            case 0:
-//                 ratingImage.setImageResource(R.drawable.zero_stars);
-//            case 1:
-//                 ratingImage.setImageResource(R.drawable.one_star);
-//            case 2:
-//                 ratingImage.setImageResource(R.drawable.two_stars);
-//            case 3:
-//                 ratingImage.setImageResource(R.drawable.three_stars);
-//            case 4:
-//                 ratingImage.setImageResource(R.drawable.four_stars);
-//            case 5:
-//                 ratingImage.setImageResource(R.drawable.five_stars);
-//            default:
-//                 ratingImage.setImageResource(R.drawable.five_stars);
-//        }
+        int rating = Math.round(extras.getInt("rating"));
+        switch (rating) {
+            case 0:
+                 ratingImage.setImageResource(R.drawable.zero_stars);
+            case 1:
+                 ratingImage.setImageResource(R.drawable.one_star);
+            case 2:
+                 ratingImage.setImageResource(R.drawable.two_stars);
+            case 3:
+                 ratingImage.setImageResource(R.drawable.three_stars);
+            case 4:
+                 ratingImage.setImageResource(R.drawable.four_stars);
+            case 5:
+                 ratingImage.setImageResource(R.drawable.five_stars);
+            default:
+                 ratingImage.setImageResource(R.drawable.five_stars);
+        }
 
-//        int currPop = (int) Math.round(restaurant.getCurrent_popularity());
-//        switch (currPop) {
-//            case 0:
-//                populationImage.setImageResource(R.drawable.safe_guy);
-//            case 1:
-//                populationImage.setImageResource(R.drawable.lonely_guy);
-//            case 2:
-//                populationImage.setImageResource(R.drawable.two_guys);
-//            case 3:
-//                populationImage.setImageResource(R.drawable.three_guys);
-//            case 4:
-//                populationImage.setImageResource(R.drawable.four_guys);
-//            case 5:
-//                populationImage.setImageResource(R.drawable.five_guys);
-//            default:
-//                populationImage.setImageResource(R.drawable.five_guys);
-//        }
+        int currPop = extras.getInt("curr_pop") / 20;
+        switch (currPop) {
+            case 0:
+                populationImage.setImageResource(R.drawable.safe_guy);
+            case 1:
+                populationImage.setImageResource(R.drawable.lonely_guy);
+            case 2:
+                populationImage.setImageResource(R.drawable.two_guys);
+            case 3:
+                populationImage.setImageResource(R.drawable.three_guys);
+            case 4:
+                populationImage.setImageResource(R.drawable.four_guys);
+            case 5:
+                populationImage.setImageResource(R.drawable.five_guys);
+            default:
+                populationImage.setImageResource(R.drawable.five_guys);
+        }
 
         TextView name = new TextView(this);
-//        name.setText("Name: " + restaurant.getName);
+        name.setText("Name: " + extras.getString("name"));
         txtLayout.addView(name);
 
         TextView address = new TextView(this);
-//        address.setText("Address: " + restaurant.getAddress);
+        address.setText("Address: " + extras.getString("address"));
         txtLayout.addView(address);
     }
 }
