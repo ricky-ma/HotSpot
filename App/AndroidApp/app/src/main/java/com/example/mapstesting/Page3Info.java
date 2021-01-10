cd019bca8bf78795d8a0a4865cec34ffe2cb6e8a
Kaleb <kaleb.e.hui@gmail.com>
2021-01-10 01:36:25 -0800

    fixed Manifest, added some functionality on Page3

--- App/AndroidApp/app/src/main/java/com/example/mapstesting/Page3Info.java
+++ App/AndroidApp/app/src/main/java/com/example/mapstesting/Page3Info.java
@@ -1,5 +1,7 @@
 package com.example.mapstesting;
 
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +12,45 @@ public class Page3Info extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
 
+        LinearLayout mainLayout = findViewById(R.id.ButtonLayout);
 
+        //display number of stars for restaurant
+//        int rating = (int) Math.round(restaurant.getRating());
+//        switch (rating) {
+//            case 1:
+//
+//            case 2:
+//
+//            case 3:
+//
+//            case 4:
+//
+//            case 5:
+//
+//            default:
+//        }
+
+//        int currPop = (int) Math.round(restauraunt.getCurrent_popularity());
+//        switch (currPop) {
+//            case 1:
+//
+//            case 2:
+//
+//            case 3:
+//
+//            case 4:
+//
+//            case 5:
+//
+//            default:
+//        }
+
+        TextView name = new TextView(this);
+//        name.setText("Name: " + restaurant.getName);
+        mainLayout.addView(name);
+
+        TextView address = new TextView(this);
+//        address.setText("Address: " + restaurant.getAddress);
+        mainLayout.addView(address);
     }
 }
